db929acd30e140715cec67d8602e369c1dce55c3
nataly2102 <nataly.2001.espinosa@gmail.com>
2022-07-19 06:25:42 +0000

    Listooooo

---
+++ code/main.py
@@ -0,0 +1,64 @@
+from fastapi import Depends, FastAPI, HTTPException, status, Security
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import pyrebase
+
+app = FastAPI()
+@app.get('/',
+summary="Api-Rest")
+async def get():
+    return "API-REST 2.0"
+
+firebaseConfig = {
+    'apiKey': "AIzaSyC6UDpFIBmgveN0BQq1tK6WZH60hnOOn0E",
+    'authDomain': "fir-api-70f5c.firebaseapp.com",
+    'databaseURL': "https://fir-api-70f5c-default-rtdb.firebaseio.com",
+    'projectId': "fir-api-70f5c",
+    'storageBucket': "fir-api-70f5c.appspot.com",
+    'messagingSenderId': "141856431030",
+    'appId': "1:141856431030:web:2e796328394b9afd07bf17"
+}
+firebase = pyrebase.initialize_app(firebaseConfig)
+securityBasic = HTTPBasic()
+securityBearer = HTTPBearer()
+@app.get("/user/token/",
+         status_code =status.HTTP_202_ACCEPTED,
+         summary     ="Get a token a user",
+         description ="Get a token for a user",
+         tags=["auth"])
+def get_token(credentials: HTTPBasicCredentials = Depends(securityBasic)):
+    try:
+        email    = credentials.username
+        password = credentials.password
+        auth     = firebase.auth()
+        user     = auth.sign_in_with_email_and_password(email, password)
+        response = {
+            "token": user["idToken"]
+        }
+        return response
+    except Exception as error:
+        print(f"Error : {error}")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+@app.get("/users/",
+         status_code=status.HTTP_202_ACCEPTED,
+         summary     ="Get a  user",
+         description ="Get a user",
+         tags=["auth"])
+
+async def get_user(credentials: HTTPAuthorizationCredentials = Depends(securityBearer)):
+    try:
+        auth = firebase.auth()
+        user = auth.get_account_info(credentials.credentials)
+        uid = user['users'][0]['localId']
+
+        db = firebase.database()
+        user_data = db.child("users").child(uid).get().val()
+
+        response = {
+            'user_data': user_data
+        }
+        return response
+    except Exception as error:
+        print(f"Error: {error}")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
